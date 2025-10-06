70721441d0c70fd702a06257d18334b566233ec0
Chúc Linh <chucv1995@gmail.com>
2025-10-06 15:20:03 +0700

    Update InAndOutDegree.cpp

--- InAndOutDegree.cpp
+++ InAndOutDegree.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include<vector>
-#include <map>
-#include <set>
+
 using namespace std;
 
 int main() {
@@ -38,6 +37,7 @@ int main() {
 
     return 0;
 }
+
 
 
 
