6634f25571d6131b4af2ae90593ef4543df1994e
Chúc Linh <chucv1995@gmail.com>
2025-11-10 17:02:05 +0700

    Add files via upload

---
+++ CheckBipariteGraph/CheckBipartiteGraph.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+vector<int> adj[100];
+int parent[100];
+
+void dsc()
+{
+	cin>>n>>m;
+	for(int i=0;i<m;i++)
+	{
+		int x,y;
+		cin>>x>>y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+	memset(parent,-1, sizeof(parent));
+}
+int BFS(int u)
+{
+	queue <int> a;
+	a.push(u);
+	parent[u]=0;
+	while(!a.empty())
+	{
+		int v=a.front();
+		a.pop();
+		for(int x:adj[v])
+		{
+			if(parent [x] ==-1)
+			parent[x] =1 - parent[v];
+			else if(parent [x] == parent[v])
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+int main()
+{
+	dsc();
+	if(!BFS(1))
+		cout<<" NOT Bipartite Graph";
+		else cout<< "It's a Bipartite Graph";
+	return 0;
+}
